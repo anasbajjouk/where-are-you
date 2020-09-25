84dfd78514de8c8dab66ad7783ce8475ee40caf9
anasbajjouk <a.bajjouk@cleo.one>
2020-09-25 15:58:59 +0200

    Updates

---
+++ precache-manifest.c60cd815bf1cfafcc502dd84198832f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2299a9f11b45daa30c4cad3b085fb2e5",
+    "url": "/where-are-you/index.html"
+  },
+  {
+    "revision": "2fa41cd949e5eb4d6053",
+    "url": "/where-are-you/static/css/main.67aff345.chunk.css"
+  },
+  {
+    "revision": "b1128ac0cb63b4cd48cc",
+    "url": "/where-are-you/static/js/2.2b467ce2.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/where-are-you/static/js/2.2b467ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa41cd949e5eb4d6053",
+    "url": "/where-are-you/static/js/main.305e12d5.chunk.js"
+  },
+  {
+    "revision": "8f750c6bc8e706941857",
+    "url": "/where-are-you/static/js/runtime-main.5027981b.js"
+  },
+  {
+    "revision": "432140243b5a3e4f0d3fb7edd53548f0",
+    "url": "/where-are-you/static/media/text.43214024.png"
+  }
+]);
